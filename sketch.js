f4282b24808a58870db1d77b7ef783f4d8455fcf
Akshat Sharma <69390481+AkshatSharma704@users.noreply.github.com>
2020-09-19 13:28:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine=Matter.Engine; 
+const World=Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground;
+
+function setup() {
+  createCanvas(800,400);
+  
+  world = World;
+  engine = Engine;
+
+  ground = new Ground(0, 400, 400, 10);
+}
+
+function draw() {
+  background(255,255,255);
+  
+  ground.display();
+  drawSprites();
+}
